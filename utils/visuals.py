6078382cf8a86dd0da50ceb43889950cc08bb053
Pinki <pinkas.matej@gmail.com>
2025-09-23 22:19:04 +0200

    added visuals file to repository

---
+++ utils/visuals.py
@@ -0,0 +1,19 @@
+__author__ = "Pinkas Matěj - Pinki"
+__maintainer__ = "Pinkas Matěj - Pinki"
+__email__ = "pinkas.matej@gmail.com"
+__credits__ = []
+__created__ = "23/09/2025"
+__date__ = "23/09/2025"
+__status__ = "Prototype"
+__version__ = "0.1.0"
+__copyright__ = ""
+__license__ = ""
+
+"""
+Project: NetLink
+Filename: visuals.py
+Directory: utils/
+"""
+
+if __name__ == '__main__':
+    pass
